7b4183d12fe7e73c9dc0b071fc38621c540de4a2
Ujwal Rakesh Pandey <ujwalrpandey7718@gmail.com>
2026-02-03 02:27:18 +0530

    Add header top layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import HeaderTop from "@/components/HeaderTop";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -22,6 +23,7 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <HeaderTop  />
         {children}
       </body>
     </html>
