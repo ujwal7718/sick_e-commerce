1bf7c1ef50336c92d8c15e118e12c783f40fb68b
Ujwal Rakesh Pandey <ujwalrpandey7718@gmail.com>
2026-02-04 02:54:19 +0530

    fixed main header

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import HeaderTop from "@/components/HeaderTop";
+import HeaderMain from "@/components/HeaderMain";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -24,6 +25,7 @@ export default function RootLayout({ children }) {
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <HeaderTop  />
+        <HeaderMain />
         {children}
       </body>
     </html>
